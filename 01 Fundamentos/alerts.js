c8207014f8ebd33dae3131fa7d7fc915428e7084
KevSosa <eli.dani.unah95@gmail.com>
2021-09-27 21:57:45 -0600

    Subiendo primeros archivos de Javacript

---
+++ 01 Fundamentos/alerts.js
@@ -0,0 +1,9 @@
+// alert('Hola Mundo')
+
+// let nombre = prompt('¿Cual es su nombre?', 'Escriba su nombre')
+
+// console.log(nombre)
+
+const selec1 = confirm('¿Esta seguro de cancelar?')
+
+console.log(selec1)
